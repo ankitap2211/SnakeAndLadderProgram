7ee0a05dfb1f83e0305b554157b484eec265a44b
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-17 07:37:11 +0530

    ADDED: Player start at 0 position

--- src/SnakeAndLadderProgram.java
+++ src/SnakeAndLadderProgram.java
@@ -1,5 +1,7 @@
 public class SnakeAndLadderProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program");
+        int position = 0;
+        System.out.println("You are in " + position + " position");
     }
 }
