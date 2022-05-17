8db470b2c60ca3b862023e77a214480eefb822f3
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-17 07:47:06 +0530

    ADDED: Player to check option

--- src/SnakeAndLadderProgram.java
+++ src/SnakeAndLadderProgram.java
@@ -6,5 +6,18 @@ public class SnakeAndLadderProgram {
 
         int diecheck = 1 + (int) (Math.random() * 6);
         System.out.println("The number on dies is :" + diecheck);
+
+        int option = (int) (Math.random() * 4);
+
+        switch (option) {
+            case 1:
+                System.out.println("No play: Player in same position");
+                break;
+            case 2:
+                System.out.println("Ladder: Player moves ahead");
+                break;
+            case 3:
+                System.out.println("Snake: Player moves behind");
+        }
     }
 }
