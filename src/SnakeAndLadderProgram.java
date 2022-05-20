9514e45b00dca26d25b4deb2b67c6a1be9b77464
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-20 21:08:00 +0530

    ADDED: Winning position

--- src/SnakeAndLadderProgram.java
+++ src/SnakeAndLadderProgram.java
@@ -1,12 +1,18 @@
 public class SnakeAndLadderProgram {
+    public static final int position = 0;
+    public static final int option;
+
     public static void main(String[] args) {
+        int startposition = 0;
+        int NO_PLAY = 0;
+        int IS_LADDER = 1;
+        int IS_SNAKE = 2;
         System.out.println("Welcome to Snake and Ladder Program");
-        int position = 0;
+        System.out.println("Starting position of player is " + startposition);
         System.out.println("You are in " + position + " position");
 
         int diecheck = 1 + (int) (Math.random() * 6);
         System.out.println("The number on dies is :" + diecheck);
-
         int option = (int) (Math.random() * 4);
 
         switch (option) {
@@ -18,6 +24,29 @@ public class SnakeAndLadderProgram {
                 break;
             case 3:
                 System.out.println("Snake: Player moves behind");
+        }
+        while (position < 100) {
+            int dieNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println("Dice number is " + dieNumber);
+
+            int option = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println("Player option is " + option);
+
+            switch (option) {
+                case 1:
+                    position = dieNumber + position;
+                    System.out.println("Position of the player after ladder is " + position);
+                    break;
+                case 2:
+                    position = position - dieNumber;
+                    if (position < 0) {
+                        position = 0;
+                    }
+                    System.out.println("Position of the player after the snake is " + position);
+                    break;
+                default:
+                    System.out.println("No play");
+            }
         }
     }
 }
